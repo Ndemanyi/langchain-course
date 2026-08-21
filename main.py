9db3ae9c8ce37fd827dc25be376755b88b7331b1
Adolphe Ndemanyi <ndemanyi@gmail.com>
2026-08-21 15:47:08 -0400

    feat(search-agent): add Tavily-powered job search agent

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from  langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+
+llm = ChatOpenAI(model="gpt-5")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
+
+
 def main():
     print("Hello from langchain-course!")
+    result = agent.invoke({"messages": HumanMessage(content="search for job postings for data engineer in quebec city using langchain on linkedin and list their details")})
+    print(f"Agent result: {result}")    
 
 
 if __name__ == "__main__":
